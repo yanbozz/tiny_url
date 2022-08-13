db199d4122c888ea7e4310fc160e0a57daafa9e3
yanbozz <yanbozhao1994@gmail.com>
2022-08-13 14:32:49 +0800

    add homepage redirect

--- tiny_url/urls.py
+++ tiny_url/urls.py
@@ -15,11 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
+from django.views.generic import RedirectView
 from django.urls import path, include
 from users import views as user_views
 from shorteners.views import VisitUrlView
 
+
 urlpatterns = [
+    path('', RedirectView.as_view(url='/shorteners/list')),
     path('auth/admin/', admin.site.urls),
     path('auth/signup/', user_views.signup, name='signup'),
     path('shorteners/', include('shorteners.urls')),
